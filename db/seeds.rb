9070eb5cf67ea33596787631877f788fb7830f5d
katsuki <kh.sph.567890@gmail.com>
2016-05-18 22:13:24 +0900

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require "csv"
+
+checkups_csv = CSV.readlines("db/checkups.csv")
+checkups_csv.shift
+checkups_csv.each do |row|
+  Checkup.create(hospital_id: row[1], name: row[2], detail: row[3], price: row[4], coupon: row[7],)
+end
