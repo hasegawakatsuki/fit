7039d105fd85bf7c0e9805f97f7dbb694ccf3794
katsuki <kh.sph.567890@gmail.com>
2016-05-18 22:17:41 +0900

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,14 @@
 
 require "csv"
 
-checkups_csv = CSV.readlines("db/checkups.csv")
-checkups_csv.shift
-checkups_csv.each do |row|
-  Checkup.create(hospital_id: row[1], name: row[2], detail: row[3], price: row[4], coupon: row[7],)
+# checkups_csv = CSV.readlines("db/checkups.csv")
+# checkups_csv.shift
+# checkups_csv.each do |row|
+#   Checkup.create(hospital_id: row[1], name: row[2], detail: row[3], price: row[4], coupon: row[7],)
+# end
+
+companies_csv = CSV.readlines("db/companies.csv")
+companies_csv.shift
+companies_csv.each do |row|
+  Company.create(name: row[1], homepage: row[2], access: row[5], logo_url: row[6], entrance_fee: row[7], detail: row[8])
 end
